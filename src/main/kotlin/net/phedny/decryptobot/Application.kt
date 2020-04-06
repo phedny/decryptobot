221e79b60d6e747a035e1e1a7c9faf8a77b3a2ed
Mark van Cuijk <github@mark.van-cuijk.nl>
2020-04-06 10:34:11 +0200

    Disable spreadsheet copy at startup

--- src/main/kotlin/net/phedny/decryptobot/Application.kt
+++ src/main/kotlin/net/phedny/decryptobot/Application.kt
@@ -3,9 +3,9 @@ package net.phedny.decryptobot
 import kotlin.random.Random
 
 fun main() {
-    val gameId = Random.nextInt()
-    val sheetsClient = SheetsClient()
-    println("New spreadsheet @ ${sheetsClient.initializeNewSpreadsheet(gameId)}")
+//    val gameId = Random.nextInt()
+//    val sheetsClient = SheetsClient()
+//    println("New spreadsheet @ ${sheetsClient.initializeNewSpreadsheet(gameId)}")
 
     val token = System.getenv("DISCORD_BOT_TOKEN") ?: throw IllegalStateException("Bot token not provided")
     Launcher(token).launch()
