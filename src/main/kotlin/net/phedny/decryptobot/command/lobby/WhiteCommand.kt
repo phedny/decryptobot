05135f623502be2c9f71b8b7ceacce0ee370911e
Mark van Cuijk <github@mark.van-cuijk.nl>
2020-04-07 17:18:31 +0200

    Smile for white team

--- src/main/kotlin/net/phedny/decryptobot/command/lobby/WhiteCommand.kt
+++ src/main/kotlin/net/phedny/decryptobot/command/lobby/WhiteCommand.kt
@@ -8,7 +8,7 @@ import net.phedny.decryptobot.state.LobbyRepository
 class WhiteCommand: Command {
     override fun execute(event: GuildMessageReceivedEvent, prefix:String) {
         if (LobbyRepository.updateLobby(event.guild.id, event.channel.id, whitePlayers = event.message.mentionedMembers.map { it.id })) {
-            event.channel.send("A white team with ${event.message.mentionedMembers.size} players, that sounds like fun!")
+            event.channel.send("A white team with ${event.message.mentionedMembers.size} players, that sounds like fun! :smile:")
         } else {
             event.channel.send("I'm not activated in this channel. Want to prepare a Decrypto team? First activate me using the `!decrypto` command :+1:")
         }
